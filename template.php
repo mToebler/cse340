262cfebb3bd9a12868d2b7b17bc761d86c7c8e52
Mark Tobler <marktobler@gmail.com>
2021-04-30 10:57:36 -0700

    first commit: template in progress

---
+++ template.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+   <title>CSE340 Template</title>
+   <meta charset="utf-8">
+   <meta name="description" content="Template for CSE340 future pages">
+   <meta name="author" content="Mark Tobler">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css2?family=New+Tegomin&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+   <link rel="stylesheet" href="css/phpmotors.css" media="screen">
+
+</head>
+<body>
+<header>
+   <?php include 'pages/header.php';?>
+</header>
+<nav>
+   <?php include 'pages/nav.php';?>
+</nav>
+<main>
+   Main content!
+</main>
+<footer>
+   <?php include 'pages/footer.php';?>
+</footer>
+</body>
+</html>
+
+
