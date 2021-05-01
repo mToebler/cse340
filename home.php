808871d532ed389b0aef1f681b9c1d991668c0d6
Mark Tobler <marktobler@gmail.com>
2021-05-01 11:45:57 -0700

    finishing touches

--- home.php
+++ home.php
@@ -5,9 +5,9 @@
 
 <html lang="en">
 <head>
-   <title>CSE340 Template</title>
+   <title>PHP Motors</title>
    <meta charset="utf-8">
-   <meta name="description" content="Template for CSE340 future pages">
+   <meta name="description" content="PHP Motors">
    <meta name="author" content="Mark Tobler">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="preconnect" href="https://fonts.gstatic.com">
